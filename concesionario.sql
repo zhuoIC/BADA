587339cc7af394f340e1d191b26cd60c31a49fa4
Nico <nicolimonar@gmail.com>
2017-06-01 20:15:55 +0200

    Organizando en carpetas. Añadida teoria 1 Evaluacion

--- concesionario.sql
+++
@@ -1,3 +0,0 @@
---Ejercicio 17
-select ciu.*, co.* from (select * from clientes where ciudad = 'madrid') as ciu join (select * from concesionario where ciudad = 'madrid') as co;
-
--- fechaYAleatorio.sql
+++
@@ -1,9 +0,0 @@
-delimiter //
-
-create procedure fechaYAleatorio()
-not deterministic
-begin
-	current_date(), rand;
-end //
-
-delimiter ;
